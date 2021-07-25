f28047382e1d13819c68596df5788cf04685509f
Charlie Chiang <charlie_c_0129@outlook.com>
2021-07-25 16:59:08 +0800

    feat: make operations modular, allow more customizations

--- runner_scripts_dev/start.sh
+++ runner_scripts_dev/start.sh
@@ -1,7 +1,21 @@
-echo "hello from dev"
-# ↑↑↑↑ command up here ↑↑↑ 
+#!/bin/bash
 
-# Write the command to start you application on the first line
-# **ONLY** the first line will be used
+echo -e "${GREY}\tHello from dev. Start you app here.${OFF}"
 
-# For example, you may start a Java application using `java -jar example.jar`
+# Below is an example that
+# will start a process in the background (if ${DETACH} is true, otherwise in the foreground)
+# and will save the PID of that process so that we can stop it later (see stop.sh)
+
+# The actual command to start your app process
+START_COMMAND="sleep 10"
+
+if [ "${DETACH}" = true ]; then
+    # This will start the process and store the pid of the process
+    START_COMMAND_PID="${START_COMMAND} & echo \$! > ./${PID_FILE_NAME} &"
+    # This will let it run in the background
+    START_COMMAND_NOHUP="nohup bash -c '${START_COMMAND_PID}' > start.out 2> start.err < /dev/null &"
+    eval "${START_COMMAND_NOHUP}"
+else
+    # This will let it run in the foreground
+    ${START_COMMAND} || exit 1
+fi
--- runner_scripts_dev/update.sh
+++ runner_scripts_dev/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "${GREY}\tHello from dev. Update you app here.${OFF}"
+
 # This is the script you use to update you project.
 # For example, you may want to add some secret key to your source code
 # that is not in the public Git repository due to security reasons.
